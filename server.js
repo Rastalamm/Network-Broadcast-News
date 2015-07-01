fa45fe1547e1175493949dc89f77e47124aeec2f
Judah Lamm <judah@Judahs-MacBook-Pro.local>
2015-06-30 21:07:58 -1000

    got an array of objects with sockets

--- server.js
+++ server.js
@@ -4,19 +4,26 @@ var PORT = 6969;
 var HOST = '0.0.0.0';
 var FILE_NAME = 'server.log';
 
+var connectedSockets = [];
 
 
 
 function clientConnected (socket) {
 
   console.log('client connected');
+  console.log('port', socket.remotePort);
+  console.log('address', socket.remoteAddress);
 
+  connectedSockets.push({
+    socket : socket,
+    port : socket.remotePort,
+    address : socket.remoteAddress })
+
+  console.log('arr', connectedSockets);
   socket.on('end', function() {
     console.log('client disconnected');
   });
 
-  console.log('port', socket.remotePort);
-  console.log('address', socket.remoteAddress);
 
   socket.on('data', function(data){
 
@@ -30,6 +37,7 @@ function clientConnected (socket) {
 }
 
 var server = net.createServer(clientConnected);
+
 server.listen(PORT, HOST, function() { //'listening' listener
   console.log('server port is', PORT);
 });
