27fc129c4816545510dcf17199b359e6c43bbbd0
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-01 10:53:15 -1000

    got it going back and forth

--- client.js
+++ client.js
@@ -3,15 +3,30 @@ var PORT = 6969;
 var HOST = '0.0.0.0';
 var FILE_NAME = 'server.log';
 
-var socket = net.Socket({
-  readable: true,
-  writable: true
-});
+// var socket = net.Socket({
+//   readable: true,
+//   writable: true
+// });
+
+var socket = net.connect({host : HOST, port : PORT}, connectedToServer);
+
+function connectedToServer(){
+  console.log('connected to the server guys');
+  process.stdin.pipe(socket);
+
+  socket.on('data', function(data){
+    process.stdout.write('SERVER BCAST FROM '+ socket.localAddress +":"+ socket.localPort + ': ' )
+    process.stdout.write(data);
+  })
 
 
-process.stdin.pipe(socket);
+
+}
 
 
-socket.connect(PORT);
-socket.write('string for now \n');
-socket.end();
+
+
+
+
+// socket.write('string for now \n');
+// socket.end();
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var net = require('net');
 var PORT = 6969;
 var HOST = '0.0.0.0';
@@ -10,27 +9,34 @@ var connectedSockets = [];
 
 function clientConnected (socket) {
 
-  console.log('client connected');
-  console.log('port', socket.remotePort);
-  console.log('address', socket.remoteAddress);
+  // console.log('client connected');
+  // console.log('port', socket.remotePort);
+  // console.log('address', socket.remoteAddress);
+
+
+  process.stdout.write('CONNECTED: ' + socket.remoteAddress +":"+ socket.remotePort + '\n');
 
   connectedSockets.push({
     socket : socket,
     port : socket.remotePort,
     address : socket.remoteAddress })
 
-  console.log('arr', connectedSockets);
-  socket.on('end', function() {
-    console.log('client disconnected');
-  });
-
-
   socket.on('data', function(data){
-
+    process.stdout.write('SERVER BCAST FROM '+ socket.remoteAddress +":"+ socket.remotePort + ': ' )
     process.stdout.write(data);
-
+    process.stdout.write('\n');
+    // process.stdin.pipe(socket);
   })
 
+  socket.on('data', function(data){
+    socket.write(data);
+  })
+
+//loop through socket array and write to each of them
+
+    socket.on('end', function() {
+    console.log('client disconnected');
+  });
 
 
 
@@ -39,5 +45,6 @@ function clientConnected (socket) {
 var server = net.createServer(clientConnected);
 
 server.listen(PORT, HOST, function() { //'listening' listener
-  console.log('server port is', PORT);
+  process.stdout.write('Server Listening on ' + HOST + ':' + PORT + '\n');
+
 });
